f0b88cb4eae879e4eeb4e96918547cdad8bbfd53
2510aaae4 <45693357+2510aaae4@users.noreply.github.com>
2025-01-23 19:57:42 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -8,7 +8,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change-in-production'
     
     # 設置數據庫
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    if DATABASE_URL and DATABASE_URL.startswith('postgres://'):
+        DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
+    
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     
     # 設置是否追踪數據庫修改
